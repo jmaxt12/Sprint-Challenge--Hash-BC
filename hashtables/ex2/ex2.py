68aa276cca137c1299d785cfd1e0b46c194f3055
Jake Thomas <jmaxt12@gmail.com>
2019-07-12 10:57:14 -0600

    add notes and insert into hashtable for ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,14 +10,37 @@ class Ticket:
     def __init__(self, source, destination):
         self.source = source
         self.destination = destination
+    
+    # source - starting airport
+    # destination - next airport
+
+'''
+tickets = [
+  Ticket{ source: "PIT", destination: "ORD" },
+  Ticket{ source: "XNA", destination: "CID" },
+  Ticket{ source: "SFO", destination: "BHM" },
+  Ticket{ source: "FLG", destination: "XNA" },
+  Ticket{ source: "NONE", destination: "LAX" },
+  Ticket{ source: "LAX", destination: "SFO" },
+  Ticket{ source: "CID", destination: "SLC" },
+  Ticket{ source: "ORD", destination: "NONE" },
+  Ticket{ source: "SLC", destination: "PIT" },
+  Ticket{ source: "BHM", destination: "FLG" }
+]
+
+output should look like =>
+["LAX", "SFO", "BHM", "FLG", "XNA", "CID", "SLC", "PIT", "ORD"]
+'''
 
 
 def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
 
-    """
-    YOUR CODE HERE
-    """
+# key => source
+# value => destination
+
+    for i in range(length):
+        hash_table_insert(hashtable, tickets[i].source, tickets[i].destination)
 
     return route
