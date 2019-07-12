f20ff53a6753dcf771e830d0fc8888be6d9f4fb2
Jake Thomas <jmaxt12@gmail.com>
2019-07-12 10:25:08 -0600

    ex1 working

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -9,15 +9,27 @@ from hashtables import (HashTable,
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
-    """
-    YOUR CODE HERE
-    """
+    #   weights - a list of integers
+    #   length - the length of the list
+    #   limit - the integer to solve for 
 
+#   2 nested loops
+
+    for i in range(length):
+        hash_table_insert(ht, weights[i], i)
+    
+    for i in range(length):
+        weight = hash_table_retrieve(ht, limit-weights[i])
+        if weight != None:
+            return (i, weight) if i > weight else (weight, i)
+            
     return None
 
 
 def print_answer(answer):
-    if answer is None:
+    if answer is not None:
         print(str(answer[0] + " " + answer[1]))
     else:
         print("None")
+
+print(get_indices_of_item_weights([1,2,3], 3, 4))
